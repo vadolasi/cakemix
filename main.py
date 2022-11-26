52bc84bccfb4f242852fdab763af1a5d320c3b9f
Vitor Daniel <vitor036daniel@gmail.com>
2022-11-25 22:09:58 -0300

    add files create

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
+from tempfile import TemporaryDirectory
+from pathlib import Path
+from jinja2 import Template
+
 import typer
+from git import Repo
 
 app = typer.Typer()
 
 
-@app.command()
-def hello(name: str):
-    print(f"Hello {name}")
+def list_paths(root_tree, path=Path(".")):
+    for blob in root_tree.blobs:
+        yield path / blob.name
+    for tree in root_tree.trees:
+        yield from list_paths(tree, path / tree.name)
 
 
-@app.command()
-def goodbye(name: str, formal: bool = False):
-    if formal:
-        print(f"Goodbye Ms. {name}. Have a good day.")
-    else:
-        print(f"Bye {name}!")
+def main(cakemix_path: str, output_path: str):
+    with TemporaryDirectory() as tempdir:
+        repo = Repo.clone_from(cakemix_path, tempdir)
+
+        commit = repo.head.commit
+
+        for path in list_paths(commit.tree):
+            file = Path(tempdir, path)
+
+            try:
+                text = file.read_text()
+
+                template: Template = Template(text)
+                data = template.render({ "a": "b" })
+
+                result = Path(output_path, path)
+
+                result.parent.mkdir(parents=True, exist_ok=True)
+                result.write_text(data)
+
+            except:
+                continue
 
 
 if __name__ == "__main__":
-    app()
+    typer.run(main)
